de5428a5442230c0f3247615bee18eee31dd407f
MullerBrogiolo <atbrogiolo@mail.austral.edu.ar>
2025-03-28 10:58:00 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,19 @@
 def math():
     a = 57
     b = 7
+
+    suma = a + b
+    diferencia = a - b
+    producto = a * b
+    promedio = (a + b) / 2
+    cociente_entero = a // b
+    resto_division = a % b
+    valor_real_division = a / b
+
+    print(suma)
+    print(diferencia)
+    print(producto)
+    print(promedio)
+    print(cociente_entero)
+    print(resto_division)
+    print(valor_real_division)
